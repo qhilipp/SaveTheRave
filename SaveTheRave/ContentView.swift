031a4f1040e161e12f6eafc247d5dc8786b6936e
Philipp Kathöfer <philippkathoefr@gmail.com>
2025-01-11 09:31:42 -0800

    Initial Commit

---
+++ SaveTheRave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaveTheRave
+//
+//  Created by Philipp Kathöfer on 11.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SaveTheRave/SaveTheRaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheRaveApp.swift
+//  SaveTheRave
+//
+//  Created by Philipp Kathöfer on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheRaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
