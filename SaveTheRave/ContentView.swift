179dee5251a5742ee54f7a7384f2280051fc10de
jmjumper <juli.springer@gmx.de>
2025-01-11 15:04:40 -0800

    Added basic QR-Code logic

--- SaveTheRave/ContentView.swift
+++ SaveTheRave/ContentView.swift
@@ -21,6 +21,9 @@ struct ContentView: View {
 			Tab("Connections", systemImage: "person.3.fill") {
 				ConnectionsView(profile: profile)
 			}
+            Tab("QR-Code", systemImage: "qrcode.viewfinder") {
+                QRGeneratorView()
+            }
 		}
 		.sheet(isPresented: $showWelcomePipeline) {
 			WelcomePipelineView()
